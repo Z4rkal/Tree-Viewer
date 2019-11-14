07cd3ddffa65f4c3ed9e0aba14720d270724a985
Michael Fischer <zarkal@live.com>
2019-11-14 14:59:58 -0800

    Added simple error catching to the app container
    Added redux and react-redux and updated some dependencies

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -6,11 +6,22 @@ class App extends Component {
     constructor() {
         super();
 
+        this.state = {
+            errorMsg: ''
+        };
+
         this.handleCanvasScroll = this.handleCanvasScroll.bind(this);
     }
 
     componentDidMount() {
+        //Add the listener for detecting in-canvas scrolling
         document.addEventListener('wheel', this.handleCanvasScroll, { passive: false });
+
+        //TODO: Calculate available screen space
+
+        //Stat tab should be left 30%? on non-mobile
+        //Canvas should be able to go up to 8k height/width and still work on any browser I'm aware of, but that seems way bigger than necessary
+        //Should go up to 2k maybe
     }
 
     componentWillUnmount() {
@@ -22,7 +33,30 @@ class App extends Component {
             event.preventDefault();
     }
 
+    componentDidCatch(error) {
+        console.log(error);
+        this.setState(() => {
+            return { errorMsg: error }
+        });
+    }
+
+    clearErr() {
+        this.setState(() => {
+            return { errorMsg: '' }
+        });
+    }
+
     render() {
+        const { errorMsg } = this.state;
+
+        if (errorMsg)
+            return (
+                <div id='app-container'>
+                    {`${errorMsg}`}
+                    <button id='clear-state' onClick={() => this.clearErr()}>Reset</button>
+                </div>
+            )
+
         return (
             <div id='app-container'>
                 <SkillTree />
